6a6afd46d2541803637c407fc6b5231e2f8e26f7
Boi Schrader <boi1992@t-online.de>
2022-06-30 14:42:25 +0200

    tasks done

---
+++ solution.js
@@ -0,0 +1,39 @@
+var snackServings = 50 ;
+var guests = 100;
+var drinks = 10;
+var resultOne = 500;
+var monkeys = 15;
+var bananas = 9;
+var monkeyfights = 20;
+var resultTwo ;
+var monkeyBusiness = 90;
+var boredom = 13;
+var happiness = 6 ;
+
+
+console.log('snackServings', snackServings)
+console.log(snackServings)
+console.log('guests')
+console.log(guests)
+console.log('snackServings - guests',)
+console.log(snackServings - guests)
+console.log('guests - snackServings')
+console.log(guests - snackServings)
+console.log('snackServings * guests')
+console.log(snackServings * guests)
+console.log('snackServings / guests')
+console.log(snackServings / guests)
+console.log('(snackServings * guests) / drinks')
+console.log((snackServings * guests) / drinks)
+console.log('resultOne')
+console.log(resultOne)
+console.log('monkeys / bananas')
+console.log(monkeys / bananas)
+console.log('(monkeys + bananas) * monkeyfights = ?')
+resultTwo = (monkeys + bananas) * monkeyfights
+console.log((monkeys + bananas) * monkeyfights)
+
+monkeyBusiness++
+console.log('monkeys / bananas increased to ' ,monkeyBusiness)
+console.log('monkeys - bananas ' ,monkeys - bananas)
+console.log('monkeys - bananas + happiness + monkeyfights ' ,monkeys - bananas + happiness + monkeyfights)
